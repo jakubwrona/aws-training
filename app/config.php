2caef06269ef83a976733902349d7ddd26521c4e
Jakub Wrona <km.wrona@gmail.com>
2017-09-08 15:20:31 +0200

    consumer added

---
+++ app/config.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+define('HOST', '34.240.27.79');
+define('PORT', 5672);
+define('USER', 'test');
+define('PASS', 'test');
+define('VHOST', '/');
+
+//If this is enabled you can see AMQP output on the CLI
+define('AMQP_DEBUG', false);
